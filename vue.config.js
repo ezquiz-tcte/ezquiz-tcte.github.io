3aa5e73752da9711dc1d80d49abeb2f6b4d9316c
shen <lplpp163@yahoo.com.tw>
2025-06-25 05:43:48 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/' // 部署在網域根目錄
 })
